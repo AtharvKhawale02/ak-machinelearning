0681fdbd7d8edcbf7eb6eb258094c6e196b8782a
Atharv Khawale <atharvkhawaleofficial@gmail.com>
2024-08-28 01:09:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,5 +19,14 @@ with st.expander('Data'):
 with st.expander('Data Visualization'):
   st.scatter_chart(data=df, x= 'Color' , y='Storage', color='Brand')
 
+# Data preparations
+with st.sidebar:
+  st.header('Input Features')
+  brand= st.selectbox('Brand', ('OPPO','SAMSUNG','ASUS','LG','realme','GIONEE','Apple','HTC','IQOO'))
+  storage= st.selectbox('Storage', ('8 GB','32 GB','64 GB','128 GB','256 GB','512 GB'))
+  rating= st.slider('Rating', 3.6, 4.9, 4.5 )
+  
+  
+
   
   
