953836f77937e8b7d208a7b4db660c9aaef73ef8
Atharv Khawale <atharvkhawaleofficial@gmail.com>
2024-08-27 23:51:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🤖 Machine Learning App')
 
 st.info('This is App Buils a Machine Learning Model! ')
+
+with st.expander('Data'):
+  st.write('**Raw data**)
+  df=pd.read_csv('https://raw.githubusercontent.com/sohamglobal/datasets/main/Flipkart_Mobiles.csv')
+  df
+  
