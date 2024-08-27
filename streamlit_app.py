8ef956c66e982a719fbd9edc643e65db437acb99
Atharv Khawale <atharvkhawaleofficial@gmail.com>
2024-08-28 00:43:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ with st.expander('Data'):
   Y
 
 with st.expander('Data Visualization'):
-  st.bar_chart(data=df, x= 'Color' , y='Storage', color='Brand', horizontal=True)
+  st.bar_chart(data=df, x= 'Color' , y='Storage', color='Brand',stack=False)
 
   
   
