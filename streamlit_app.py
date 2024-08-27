062f87e869e40323e552a5b7def9ba6966f72c10
Atharv Khawale <atharvkhawaleofficial@gmail.com>
2024-08-28 00:55:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-st.title('🤖 Machine Learning App')
+st.title('📱 Mobiles Data Analysis')
 
-st.info('This is App Buils a Machine Learning Model! ')
+st.info('This is App use a Machine Learning Model to analysize the given data ')
 
 with st.expander('Data'):
   st.write('**Raw data**')
